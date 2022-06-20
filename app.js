b3bcc73626f86dad4e4843a4099dc6f6b73879d0
machadogadiel <github@gadiel.dev>
2022-06-20 01:01:02 -0300

    second commit

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ const reader = readLine.createInterface(process.stdin, process.stdout);
 const inputProperties = []
 
 reader.on('line', (input) => input.toLowerCase() !== "sair" ? inputProperties.push(input) : reader.close());
-reader.on('close', () => console.log(inputProperties.sort()))
+reader.on('close', () => console.log(`\nResultado:
++⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯+
+${inputProperties.sort().join('\n')} 
++⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯+`))
